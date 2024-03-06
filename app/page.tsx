cc528d16f67fe298c3865392c522a01f3892e629
Own3r <darkwebsite95@gmail.com>
2024-03-06 10:26:43 +0330

    pagination added

--- app/page.tsx
+++ app/page.tsx
@@ -10,47 +10,84 @@ import Link from "next/link";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { FaSpinner } from "react-icons/fa";
 import { TicketTypeFixed } from "@/types/types";
+import Pagination from "./components/pagination";
+
+export const getTicketsLength: () => Promise<number> = async () => {
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  let tickets;
+  if (session?.user?.type === "admin") {
+    tickets = await prisma.ticket.count();
+   }else{
+    tickets = await prisma.ticket.count({
+      where: {
+        userMail: email,
+      },
+    });
+  }
+  return tickets;
+}
 
 
-export const getTickets: () => Promise<TicketTypeFixed> = async () => {
-  const session = await getServerSession(authOptions)
-  const email = session?.user?.email
+export const getTickets: (page: number) => Promise<TicketTypeFixed> = async (page: number) => {
+  const skip = page * 10; // It will be showing 10 tickets per page
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
    let tickets;
+
    if (session?.user?.type === "admin") {
-      tickets = await prisma.ticket.findMany()
+    tickets = await prisma.ticket.findMany({
+      orderBy: {
+        date: "desc"
+      },
+      skip: skip,
+      take: 10,
+    });
+
    }else{
     tickets = await prisma.ticket.findMany({
-    where: {
-      userMail: email
-    }
-  })}
+      where: {
+        userMail: email,
+      },
+      orderBy: {
+        date: "desc"
+      },
+      skip: skip,
+      take: 10,
+    });
+  }
 
   tickets = tickets.sort((a, b) => {
     if (a.date < b.date) {
-      return 1
+      return 1;
     }else{
-      return -1
+      return -1;
     }
   })
   return tickets;
 }
 
-export async function DisplayTickets(){
-  const tickets = await getTickets()
+export async function DisplayTickets(pagination: { page: string | number }){
+  const page = Number(pagination.page) - 1;
+  const tickets = await getTickets(page);
 
-  return <>{tickets?.map((ticket, i) => {
-    return <Ticket key={i} type="tickets" id={ticket.id} title={ticket.title} ticketStatus={ticket.status} department={ticket.department} date={ticket.date} priority={ticket.priority} tag={ticket.tag} length={ticket.ticket.length} />
-   })}
-    {tickets.length < 1 && <div className="flex justify-center w-[100%] mt-[20px] text-center flex-col">
-    <span className="text-[20px]">Seems like you don&apos;t have any ticket yet!</span>
-    <Link href="/new" className="w-[100%]"><button className="w-[30%] mt-[15px] bg-[#3342c5] text-[18px] rounded-[10px] p-[10px] cursor-pointer hover:bg-[#5665ee]">Create Your First Ticket</button></Link>
-    </div> }
-   </>
+  return (
+    <>
+      {tickets?.map((ticket, i) => {
+        return <Ticket key={i} type="tickets" id={ticket.id} title={ticket.title} ticketStatus={ticket.status} department={ticket.department} date={ticket.date} priority={ticket.priority} tag={ticket.tag} length={ticket.ticket.length} />
+      })}
+
+      {tickets.length < 1 && <div className="flex justify-center w-[100%] mt-[20px] text-center flex-col">
+        <span className="text-[20px]">Seems like there is no ticket here!</span>
+        <Link href="/new" className="w-[100%]"><button className="w-[30%] mt-[15px] bg-[#3342c5] text-[18px] rounded-[10px] p-[10px] cursor-pointer hover:bg-[#5665ee]">Create Your New Ticket</button></Link>
+      </div>
+      }
+    </>
+  )
 }
 
 export async function DisplayRecentTickets(){
-  const tickets = await getTickets();
-
+  const tickets = await getTickets(0);
   let rtArr = tickets.splice(0, 5) // recent tickets array, it only shows first 5 tickets
 
   rtArr = rtArr.sort((a, b) => {
@@ -67,7 +104,11 @@ export async function DisplayRecentTickets(){
     })}
   </>
 }
-const Dashboard = async () => {
+const Dashboard = async (params: { searchParams: { page: string | number }}) => {
+
+  const currentPage = Number(params.searchParams.page) || 1;
+  const ticketsLength = await getTicketsLength();
+  const pages = Math.ceil(ticketsLength / 10);
 
   return (
     <>
@@ -85,7 +126,10 @@ const Dashboard = async () => {
                 <div className="flex justify-between w-full max-w-[768px] flex-wrap flex-col md:flex-row">
                   
                   <Suspense fallback={<div className="flex justify-center w-full mt-[4rem]"><FaSpinner className="animate-spin text-[4rem]" /></div>}>
-                  <DisplayTickets />
+                  <DisplayTickets page={currentPage} />
+                  <div className="flex justify-center w-full">
+                    <Pagination pages={pages} currentPage={currentPage} />
+                  </div>
                   </Suspense>
 
                 </div>
